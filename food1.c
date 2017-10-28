843898a5f96302bba7f601b1098e2e00ea8c6963
shafaqshaikh <shafaqshaikh10@gmail.com>
2017-10-28 18:33:34 +0530

    Update food1.c

--- food1.c
+++ food1.c
@@ -12,13 +12,13 @@
 typedef struct food
 {
 	char fname[30];
-	int data[50];
-	float price[50];
-	int stock[10];
+	int data;
+	float price;
+	int stock;
 	struct food *next;
 }node;
 
-node *create(int n)   //creating a database
+node *insertfood(int n)   //creating a database
 {
 	int i;
 	node *head,*p;
@@ -67,86 +67,6 @@ void display(node *head)
 	}
 }
 
-int Insertafter(node *head)
-{
-	int kdata;
-	int flag=0;
-	node *temp,*p;
-	printf("ENTER DATA NO");
-	scanf("%d",&kdata);
-	p=(node*)malloc(sizeof(node));
-	printf("\nENTER THE  NAME OF THE FOOD:\t");
-	scanf("%s",&p->fname);
-	printf("\nENTER DATA:\t");
-	scanf("%d",&p->data);
-	printf("ENTER PRICE:\t");
-	scanf("%f",&p->price);
-	printf("ENTER STOCK:\t");
-	scanf("%d",&p->stock);
-	p->next=NULL;
-	if(head==NULL)
-	{
-		head=p;
-	}
-	else
-	{
-		temp=head;
-		while(temp!=NULL&&temp->data!=kdata)
-			temp=temp->next;
-		if(temp->data==kdata)
-		{
-
-			p->next=temp->next;
-			temp->next=p;
-			flag=1;
-		}
-	}
-	if(flag==0)
-		printf("DATA NOT FOUND");
-	return flag;
-}
-
-int delete(node *head)
-{
-	node *temp,*ptr;
-	int kdata;
-	int flag=0;
-	temp=head;
-	printf("ENTER THE DATA YOU WANT TO DELETE:\t");
-	scanf("%d",&kdata);
-	if(head!=NULL)
-	{
-	if(head->next!=NULL)
-	{
-	if(temp->data==kdata)
-	{
-		head=head->next;
-		flag=1;
-		free(head);
-	}
-	else
-	{
-		ptr=head->next;
-		while(ptr!=NULL)
-		{
-			if(ptr->data==kdata)
-			{
-				temp->next=ptr->next;
-				flag=1;
-				free(ptr);
-			}
-			temp=ptr;
-			ptr=ptr->next;
-		}
-	}
-	}
-	else if(head==NULL)
-		printf("\nNO DATA IS PRESENT");
-	}
-	if (flag==0)
-		printf("\DATA NOT FOUND");
-	return flag;
-}
 int order(node*head)
 {
 	int a;
@@ -179,11 +99,11 @@ int main()
 	do
 	{
 		printf("\n\t---------------MENU-------------------------");
-		printf("\nENTER 1 FOR CREATING A FOODLIST\n");
-		printf("\nENTER 2 FOR DISPLAYING YOUR FOODLIST\n");
-		printf("\nENTER 3 FOR INSERTING A FOOD AFTER AN ANOTHER FOOD\n");
-		printf("\nENTER 4 FOR DELETING ANY FOOD FROM THE FOODLIST\n");
-		printf("\nENTER 5 TO ORDER FOOD\n");
+		printf("\n1. FOOD MENU\n");
+		printf("\n2. ADMIN LOGIN");
+		printf("\n3. RULES AND REGULATIONS \n");
+		printf("\n4. PROBLEM AND SUGGESTIONS\n");
+		printf("\n5. ORDER YOUR FOOD\n");
 		printf("\nEnter 0 for exit\n");
 		printf("\nENTER YOUR CHOICE\n");
 		scanf("%d",&choice);
